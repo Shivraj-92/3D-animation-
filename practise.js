fa9c1f52bde0ea9a528b29eb6a520637e5d332f7
Shivraj Singh <144763437+Shivraj-92@users.noreply.github.com>
2025-06-27 15:16:30 +0530

    Add files via upload

---
+++ practise.js
@@ -0,0 +1,8 @@
+//chapter 4 practice set
+let str = "kul\""
+console.log(str.length)
+
+const sentence = 'The quick brown fox jump over the lazy dog.';
+const word = 'fox2';
+console.log(sentence.includes(word))
+console.log(`The Word "${word}" ${sentence.includes(word) ? 'is': 'is not'} in the sentence`);
